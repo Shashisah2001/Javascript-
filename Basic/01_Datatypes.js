802fe75d648d75d62c5580803fbf8af2fbc2a614
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-08 08:37:57 +0000

    Datatypes

---
+++ Basic/01_Datatypes.js
@@ -0,0 +1,34 @@
+"use strict"; // When we use this code  in javascript all the code are treated as new version.(or new js)
+/* alter (3+3) // There occur error coz we're using nodejs not a 
+        browser(But when run that cmd in console we'll get 6 as an output) */
+
+// We should avoid space in js  | Code relatibility should be high
+ console.log(3+3)
+console.log("Shashi")
+
+let name = "shashi shah"
+let Age = 22
+let isLoggedIn = true
+
+/*datatypes in Js [These are Primitive Datatypes]
+
+ number = 2 to the power 53
+
+ bigint = some time we use it for large number like it in used in stock market, facebook and so on.
+
+ boolean = Either TRUE/FALSE
+
+ string =(It should be in both ''single or "" double quotes)
+
+ null = It is stand alone value (also a type) - it is object type
+
+ undefined 
+
+ symbol = for uniqueness
+
+*/
+
+/* [Object]
+typeof = it's a value
+
+ */
