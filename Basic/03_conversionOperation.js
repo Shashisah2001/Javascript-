8c945d5acae7e79caff18d1be66b5c851a7acab3
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-08 18:59:42 +0000

    Conversion Datatype

---
+++ Basic/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 30;
+/*
+console.log(score);
+console.log(typeof score);
+//console.log(typeof(score)); //It shows the type of  score i.e :- number
+
+let valueInNumber = Number(score)
+console.log(valueInNumber); */
+
+let isLoggedIn = "1"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn);
+console.log(typeof booleanisLoggedIn);
+
+let stringNumber = String(isLoggedIn)
+console.log(stringNumber);
+console.log(typeof stringNumber);
