0fe73026323472cf61c285e79611cc8a29cb22bb
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-10 21:20:56 +0000

    Date

---
+++ Basic/07_Date.js
@@ -0,0 +1,15 @@
+let myDate = new Date();
+console.log( myDate);
+console.log( myDate.toString());
+console.log( myDate.toDateString());
+console.log( myDate.toLocaleDateString());
+console.log( myDate.toISOString());
+console.log( myDate.toISOString());
+console.log( myDate.toLocaleTimeString());
+console.log( myDate.toLocaleString());
+console.log(typeof(myDate));
+
+console.log('Hello');
+let newDate = new Date ("2-01-2023")
+console.log(newDate.toLocaleDateString());
+
