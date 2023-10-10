18fadcb358c9377002804d765d685ec8444e4499
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-10 12:55:31 +0000

    change in location

---
+++ Basic/DataType-summary.js
@@ -0,0 +1,54 @@
+ //Primitive Datatypes
+ /* 
+  All Primitive Datatypes are call by values
+  we get copy of the data not on the actual address
+ we store data on copy address  
+  Types:- 7types
+  :- string
+  :- Number
+  :- Boolean
+  :- Null
+  :- Undefined
+  :- Symbol :- it is used to make any value unique
+  :- BigInt
+
+
+JavaScript is dynamically typed.
+Variable types are determined at runtime.
+We don't have to explicitly declare the data type of a variable.
+
+
+
+
+  // Non-Primitive [or Refference] Datatypes
+ :- Array
+ :- Objects
+ :- Functions
+
+ object are always in scope({}) which is also known as curly braces
+
+ */
+
+ //const score = 69;
+ //const scoreValue ="59"; // here i stored a string value
+ //const isLoggedIn = 212121
+
+ //console.log(score);
+//  console.log(scoreValue);
+//  console.log(isLoggedIn);
+
+// const heros = ["Iron man","Shaktiman","Black Panther"];
+// console.log(heros);
+//
+let myObject = {
+    Name: "Shashi Sah",
+    Age: 22,
+}
+console.log(myObject);
+//
+
+const myFunction = function(){
+    console.log("hello Shashi");
+}
+
+console.log();
