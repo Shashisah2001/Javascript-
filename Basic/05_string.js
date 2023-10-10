0fe994ec17bdb387b4dec13381d07ac1e001c102
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-10 19:24:53 +0000

    String

---
+++ Basic/05_string.js
@@ -0,0 +1,23 @@
+const name = "Tony Stark";
+const mySuits = 80;
+//console.log(name + " have " +  mySuits + " Armor Suit");
+
+// ``:- backtick i.e String interpolation and we can do anything on the go
+
+//console.log(`${name} have ${mySuits} Suits who is most lovable Character in the whole MCU.`);
+
+const gameName =  new String("CLash of CLans");
+ console.log(gameName);
+ console.log(gameName.__proto__);
+ console.log(gameName.length);
+ console.log(gameName.toUpperCase());
+ console.log(gameName[11]);
+ console.log(gameName.charAt(2));
+ console.log(gameName.indexOf('n'));
+ console.log(gameName.substring(1,5)); // to find the position of string word
+ console.log(gameName.slice(-1,1)); // to use the negative means to count through negative side
+ 
+ const myName = new String("   Jarvis Rao  ")
+ console.log(myName);
+ console.log(myName.trim()); // to use the negative means to count through negative side
+ 
