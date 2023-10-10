7478678a1e805aef7895f2d473c8f11b0da3c101
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-10 12:15:01 +0000

    04_comparision

--- 04_comparision.js
+++ 04_comparision.js
@@ -1,1 +1,15 @@
-console.log(1<2);
+//console.log(1<2);
+
+//console.log(1>=2);
+//console.log(1==2);
+//console.log(2===2);
+//console.log("2">1);
+
+console.log(null > 0);
+console.log(null < 0); /*Above this null give false and below is true coz in js equality and  
+comparision work differently  */
+console.log(null >= 0);
+
+/*  === not only check the equality but also the datatypes*/
+
+console.log("2"===2);
