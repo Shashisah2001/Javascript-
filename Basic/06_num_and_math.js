e9e8ad79a4a9ddba50704fd1abfd0b787d44ba36
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-10 20:40:35 +0000

    Nums and Math

---
+++ Basic/06_num_and_math.js
@@ -0,0 +1,21 @@
+//const marks = 650
+//console.log(marks);
+/*
+const totalMarks = new Number(650)
+console.log(totalMarks);
+console.log(totalMarks.toString());
+console.log(totalMarks.toString().length);
+
+const totalbalance = new Number(125.586)
+console.log(totalbalance.toPrecision(5)); //it is used to roundoff the float digit to the main value
+const totalamount = new Number(3005000)
+console.log(totalamount.toLocaleString('en-IN')); // to arrange the data in standard form 
+*/
+
+//*********************Math********************************* */
+console.log(Math);
+console.log("Abstract is used to get value in positive:-" + Math.abs(-10));
+console.log("For Higher preference:-" + Math.ceil(5.5)); // ceil choose the higher preference
+console.log("For Lower preference:-"+ Math.floor(5.5));// whereas floor choose the lower preference
+console.log("For Minium value:-" + Math.min(5,10,16,2,0));
+console.log("For maximum value:-"+ Math.max(5,10,16,2,0));
