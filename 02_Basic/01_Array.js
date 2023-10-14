13602b7e61ae940b956370e068c05775f038166a
Shashi Kumar Sah <144910521+Shashisah2001@users.noreply.github.com>
2023-10-14 11:42:41 +0000

    Array1

---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,28 @@
+const myArr = [5,10,15,20,25,30,'shashi']
+//console.log(myArr);
+//console.log(myArr.includes(5)); // To find the data is available in array or not
+//console.log(myArr.indexOf(15));
+console.log(myArr[1]);
+myArr.push(5);
+myArr.pop();
+myArr.unshift(4)
+
+
+//************Method**** */
+const newArr = myArr.join() // .join is used to combined the data and convert it into string
+//console.log(newArr);
+//console.log(typeof(newArr));
+
+/*
+const myNew = myArr
+console.log(myNew);
+
+const myNew1 = myArr.slice(1,3);
+console.log("A",myNew1);
+
+const myNew2 = myArr.splice(1,3);
+console.log(myArr);
+console.log("B",myNew2);
+
+*/
+
